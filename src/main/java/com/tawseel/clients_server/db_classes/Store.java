075d4f4d1c454e0cc93905edd16699583b185b3f
Ahmadsa <ahmadedu98@hotmail.com>
2021-05-10 16:36:22 +0300

    Fixed Store.java

--- src/main/java/com/tawseel/clients_server/db_classes/Store.java
+++ src/main/java/com/tawseel/clients_server/db_classes/Store.java
@@ -1,127 +1,21 @@
 package com.tawseel.clients_server.db_classes;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "stores")
 public class Store {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(name = "name")
-    private String name;
-    @Column(name = "address")
-    private String address;
-    @Column(name = "opening_time")
-    private String openingTime;
-    @Column(name = "closing_time")
-    private String closingTime;
-    @Column(name = "phone_number")
-    private String phoneNumber;
-    @Column(name = "email")
-    private String email;
-    @Column(name = "password")
-    private String password;
-    @Column(name = "user_image")
-    private String userImage;
-    @Column(name = "post_image")
-    private String postImage;
+    @OneToMany(mappedBy ="storeID", fetch = FetchType.EAGER , cascade = CascadeType.ALL)
+    private Set<Item> items;
 
-    public Store(String name, String address, String openingTime,
-                 String closingTime, String phoneNumber, String email,
-                 String password, String userImage, String postImage) {
-        this.name = name;
-        this.address = address;
-        this.openingTime = openingTime;
-        this.closingTime = closingTime;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.password = password;
-        this.userImage = userImage;
-        this.postImage = postImage;
+    public Set<Item> getItems() {
+        return items;
     }
 
-    public Store() {
-
-    }
-
-    public String getUserImage() {
-        return userImage;
-    }
-
-    public void setUserImage(String userImage) {
-        this.userImage = userImage;
-    }
-
-    public String getPostImage() {
-        return postImage;
-    }
-
-    public void setPostImage(String postImage) {
-        this.postImage = postImage;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getOpeningTime() {
-        return openingTime;
-    }
-
-    public void setOpeningTime(String openingTime) {
-        this.openingTime = openingTime;
-    }
-
-    public String getClosingTime() {
-        return closingTime;
-    }
-
-    public void setClosingTime(String closingTime) {
-        this.closingTime = closingTime;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public void setItems(Set<Item> items) {
+        this.items = items;
     }
 }
